1c42cdecc62897a459a36b5e403fa52febda8ea3
Satyam Seth <satyam1998.1998@gmail.com>
2020-12-23 18:28:49 +0530

    add prediction template

--- App/main.py
+++ App/main.py
@@ -1,10 +1,50 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request,redirect
+import numpy as np
+import xgboost
+import pickle
+
+with open('ml_model.pkl','rb') as file:
+    classifier=pickle.load(file)
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return render_template('home.html')
+    return render_template('home.html',home_active='active')
+
+@app.route('/predict',methods=['GET','POST'])
+def predict():
+    if request.method=='POST':
+        num_preg=request.form.get('Pregnancies')
+        glucose_conc=request.form.get('Glucose')
+        diastolic_bp=request.form.get('BloodPressure')
+        thickness=request.form.get('SkinThickness')
+        insulin=request.form.get('InsulinLevel')
+        bmi=request.form.get('BodyMassIndex')
+        dpf=request.form.get('DiabetesPedigreeFunction')
+        age=request.form.get('Age')
+
+        data=np.array([[int(num_preg),int(glucose_conc),int(diastolic_bp),int(thickness),int(insulin),float(bmi),float(dpf),int(age)]])
+        
+        prediction=classifier.predict(data)
+        print(prediction)
+
+        context={
+            'num_preg':num_preg,
+            'glucose_conc':glucose_conc,
+            'diastolic_bp':diastolic_bp,
+            'thickness':thickness,
+            'insulin':insulin,
+            'bmi':bmi,
+            'dpf':dpf,
+            'age':age,
+            'pred':prediction
+        }        
+
+        return render_template('prediction.html',context=context)
+
+    elif request.method=='GET':
+        return redirect('/')
 
 if __name__ == '__main__':
 	app.run(debug=True)
